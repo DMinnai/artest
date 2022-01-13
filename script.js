0c6941d929698bf6b0ce27f27d442e383c10cc83
Davide Minnai <d.minnai@entando.com>
2022-01-13 13:16:42 +0100

    new place added

---
+++ script.js
@@ -0,0 +1,42 @@
+window.onload = () => {
+     let places = staticLoadPlaces();
+     renderPlaces(places);
+};
+
+function staticLoadPlaces() {
+    return [
+        {
+            name: 'Centro prelievi',
+            location: {
+                lat: 39.169244,
+                lng: 8.524915,
+            }
+        },
+        {
+            name: 'Palazzo Pazze',
+            location: {
+                lat: 39.169192,
+                lng: 8.526157,
+            }
+        },
+    ];
+}
+
+function renderPlaces(places) {
+    let scene = document.querySelector('a-scene');
+
+    places.forEach((place) => {
+        let latitude = place.location.lat;
+        let longitude = place.location.lng;
+
+        let model = document.createElement('a-entity');
+        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+        model.setAttribute('scale', '5 5 5');
+
+        model.addEventListener('loaded', () => {
+            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+        });
+
+        scene.appendChild(model);
+    });
+}
