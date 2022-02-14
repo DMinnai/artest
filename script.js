34ae226267b3aefd0d1aecd3f78084845140bb78
Davide Minnai <d.minnai@entando.com>
2022-02-14 20:57:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ window.onload = () => {
 
         const options = {
             method: 'get',
+            mode: 'cors',
             headers: {
                 "Content-Type": "application/json",
                 "i-edx-token": "554c8ffbe6a574c33680fb594ab8c0952b54d9301fc941a34ba27434cfd8cbca",
